b26837e70db44868e222955dc4851e3d2601092e
Ania Brajtkopf <aniabrajtkopf@gmail.com>
2017-08-08 19:43:21 +0200

    add header style

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -22,7 +22,11 @@ class App extends Component {
   }
 
   componentWillMount() {
-    
+    window.addEventListener('keydown', e => {
+      if (e.keyCode === 27) { // ESC
+        document.querySelector('.modal').classList.remove('visible');
+      }
+    });
   }
 
   componentDidMount() {
@@ -37,7 +41,7 @@ class App extends Component {
         loading: false
       });
       document.querySelector('.modal').classList.remove('visible');
-      window.scrollTo(0, document.body.scrollHeight);
+      // window.scrollTo(0, document.body.scrollHeight);
     });
     socket.on('errorMessage', error => this.setState({ error }));
   }
@@ -73,62 +77,63 @@ class App extends Component {
   render() {
     
     return (
-      <div className="container">
-        <div className="title">
-          <h1>Kursy walut</h1>
+      <div>
+        <div className="header">
+          <h1 className="header__title">Kursy walut</h1>
+          <img src={searchIcon} alt="search" onClick={() => this.openModal()} className="header__search" />
         </div>
-        <div className="search">
-          <img src={searchIcon} alt="search" onClick={() => this.openModal()} />
-        </div>
-        <div className="modal">
-          {
-            this.state.loading
-            ? <div className="loading__spinner"></div>
-            : (
-                <form className="form" onSubmit={e => this.addCode(e)}>
-                  <p className="form__close" onClick={() => this.closeModal()}>&#10005;</p>
-                  <div className="form__content">
-                    <label htmlFor="code" className="form__content__label">Kod waluty (np. USD, CHF)</label><br/>
-                    <input type="text" onChange={e => this.setState({ code: e.target.value })} name="code" style={{textTransform: "uppercase"}} ref={ref => (this.input = ref)} className="form__content__input" autoFocus />
-                    <button type="submit" className="form__content__button"><img src={searchIcon} alt="search"/></button>
+        <div className="container">
+          <div className="modal">
+            {
+              this.state.loading
+              ? <div className="loading__spinner"></div>
+              : (
+                  <form className="form" onSubmit={e => this.addCode(e)}>
+                    <p className="form__close" onClick={() => this.closeModal()}>&#10005;</p>
+                    <div className="form__content">
+                      <label htmlFor="code" className="form__content__label">Kod waluty (np. USD, CHF)</label><br/>
+                      <input type="text" onChange={e => this.setState({ code: e.target.value })} name="code" style={{textTransform: "uppercase"}} ref={ref => (this.input = ref)} className="form__content__input" autoFocus />
+                      <button type="submit" className="form__content__button"><img src={searchIcon} alt="search"/></button>
 
-                  </div>
-                </form>
+                    </div>
+                  </form>
 
-              )
-          } 
-        </div>
-        {
-          this.state.error
-        }
+                )
+            } 
+          </div>
           {
-            this.state.data
-            ? (
-                Object.keys(this.state.data).map((el, i) => {
-                  let { code, currency, rates } = this.state.data[el];
-                  
-                  return (
-                    <SingleCurr key={i} currency={currency} code={code} rates={rates} deleteCode={this.deleteCode}/>
-                  )
-                  // rates.forEach(rate => {
-                  //   labels.push(moment(rate.effectiveDate).format('DD MMM'));
-                  //   data.push(rate.mid);
-                  // });
-                  // let monthLabels = labels.slice(-(labels.length / 12)); // dwa rodzaje wykresów: rok / miesiąc
-                  // let monthData = data.slice(-(data.length / 12)); // a to przenieśc do chart
-                  // console.log(-(labels.length / 12));
-                  // return (
-                  //   <div key={i}>
-                  //     <li>{code} {currency} {Number(rates[rates.length - 1].mid).toFixed(4)}PLN {rates[rates.length - 1].effectiveDate} <span onClick={() => this.deleteCode(code)}>&#x2717;</span></li>
-                  //       <div onClick={() => this.setState({ displayChart: 'month' })}>1M</div>
-                  //       <div onClick={() => this.setState({ displayChart: 'year' })}>1R</div>
-                  //       <Chart code={code} labels={this.state.displayChart === 'month' ? monthLabels : labels} data={this.state.displayChart === 'month' ? monthData : data} points={this.state.displayChart === 'month'} /> 
-                  //   </div>  
-                  // )
-                })
-              )
-            : <div className="loading__spinner"></div>
-          } 
+            this.state.error
+          }
+            {
+              this.state.data
+              ? (
+                  Object.keys(this.state.data).map((el, i) => {
+                    let { code, currency, rates } = this.state.data[el];
+                    
+                    return (
+                      <SingleCurr key={i} currency={currency} code={code} rates={rates} deleteCode={this.deleteCode}/>
+                    )
+                    // rates.forEach(rate => {
+                    //   labels.push(moment(rate.effectiveDate).format('DD MMM'));
+                    //   data.push(rate.mid);
+                    // });
+                    // let monthLabels = labels.slice(-(labels.length / 12)); // dwa rodzaje wykresów: rok / miesiąc
+                    // let monthData = data.slice(-(data.length / 12)); // a to przenieśc do chart
+                    // console.log(-(labels.length / 12));
+                    // return (
+                    //   <div key={i}>
+                    //     <li>{code} {currency} {Number(rates[rates.length - 1].mid).toFixed(4)}PLN {rates[rates.length - 1].effectiveDate} <span onClick={() => this.deleteCode(code)}>&#x2717;</span></li>
+                    //       <div onClick={() => this.setState({ displayChart: 'month' })}>1M</div>
+                    //       <div onClick={() => this.setState({ displayChart: 'year' })}>1R</div>
+                    //       <Chart code={code} labels={this.state.displayChart === 'month' ? monthLabels : labels} data={this.state.displayChart === 'month' ? monthData : data} points={this.state.displayChart === 'month'} /> 
+                    //   </div>  
+                    // )
+                  })
+                )
+              : <div className="loading__spinner"></div>
+            } 
+
+        </div>
       </div>
     );
   }
